95087d89ce684d5b7a7d87b059274d21a95823a9
Vlad Nemanikhin <77212359+Vlad-Nemanikhin@users.noreply.github.com>
2021-08-12 00:45:45 +0300

    Баг фикс
    Добавил gallery.html в index, в index.html добавил тег main, в gallery.html добавил секцию, h1, ul. Поправил gallery.hbs.  добавил vissualy-hiden и backdrop-is-hidden в common.scss. Исправил backdrop.html, обновил функцию close-modal.js

--- src/js/close-modal.js
+++ src/js/close-modal.js
@@ -2,3 +2,19 @@ const backdrop = document.querySelector('.backdrop');
 const closeModal = () => backdrop.classList.add('modal--close');
 
 export default {backdrop, closeModal};
+
+//Пример закрытия модалки из курса по верстке
+//(() => {	
+//const refs = {
+//   openModalBtn: document.querySelector('[data-modal-open]'),
+//   closeModalBtn: document.querySelector('[data-modal-close]'),
+//   modal: document.querySelector('[data-modal]'),
+//};
+//
+//refs.openModalBtn.addEventListener('click', toggleModal);
+//refs.closeModalBtn.addEventListener('click', toggleModal);
+//
+//function toggleModal() {
+//   refs.modal.classList.toggle('backdrop--is-hidden');
+//}
+//})();
