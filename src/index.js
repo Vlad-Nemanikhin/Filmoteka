4d6ec371ef640a3cfb9260ca86cdf74ede0f2d8b
Vitalii Kovalenko <vitaliikovalenko23@gmail.com>
2021-08-12 00:43:14 +0300

    функции по поиску фильмов

--- src/index.js
+++ src/index.js
@@ -7,5 +7,78 @@ import Notiflix from "notiflix";
 import debounce from 'lodash.debounce';
 import * as APIs from './js/movies-API-service';
 
-APIs.fetchGenres();
+const gallery = document.querySelector('.gallery');
+const searchInput = document.querySelector('#search-input');
+let inputValue = '';
+
+searchInput.addEventListener('input', oninputChange);
+
+async function getGenres() {
+   try {
+       const result = await APIs.fetchGenres();
+       const genres = result.genres;
+       const id = genres.map(id => id.id);
+       const name = genres.map(name => name.name);
+       localStorage.setItem(`${id}`, `${name}`);
+       console.log(genres);
+   } catch (error) {
+       console.log('OOOOOPS!');
+   }
+}   
+
+getGenres();
+
+async function fetchTopMovies() {
+    try {
+        const response = await APIs.fetchTopMovies();
+        const movies = response.results;
+        const genres = movies.map(r => r.genre_ids);
+
+        renderGenres(genres);
+        renderTopMovies(movies);
+        console.log(movies);
+        console.log(response);
+    } catch (error) {
+        console.log('Something went wrong!');
+    }
+}
+
+fetchTopMovies();
+
+function renderTopMovies(movies) {
+    const markup = cards(movies);
+    // const releaseDate = movies.map(date => date.release_date);
+    // console.log(releaseDate);
+
+    gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+function renderGenres(genres) {
+    // console.log(genres);
+}
+
+
+
+// ----------------Функция по поиску фильма с инпута-----------------------
+
+function oninputChange(event) {
+    inputValue = event.target.value;
+    console.log(inputValue);
+}
+
+// нужно в разметке доделать кнопку с ID button-search
+const buttonSearch = document.querySelector('#button-search');
+// и повесить слушатель на нее
+buttonSearch.addEventListener('click', getMoviesByInput);
+
+async function getMoviesByInput(event) {
+    event.preventDefault();
+    try {
+        const response = await APIs.fetchMoviesByQuery(inputValue);
+        const movies = response.results;
+        console.log(movies);
+    } catch (error) {
+        console.log('ERROR');
+    }
+}
 
--- src/js/movies-API-service.js
+++ src/js/movies-API-service.js
@@ -3,19 +3,20 @@ import axios from 'axios';
 const KEY = '?api_key=130ddbced0917ef5d6e094c730cee47c';
 const QUERY = '&language=en-US&page=1&include_adult=false';
 
+
 async function fetchGenres() {
-    const responce = await axios.get(`https://api.themoviedb.org/3/genre/movie/list${KEY}&language=en-US`);
-    const genres = await responce;
-    console.log(genres.data.genres);
+    const response = await axios.get(`https://api.themoviedb.org/3/genre/movie/list${KEY}&language=en-US`);
+    const genres = await response;
+    // console.log(genres.data.genres);
     return genres.data;
 }
 
 // fetchGenres();
 
 async function fetchTopMovies() {
-    const responce = await axios.get(`https://api.themoviedb.org/3/trending/movie/day${KEY}`);
-    const movies = await responce;
-    console.log(movies.data);
+    const response = await axios.get(`https://api.themoviedb.org/3/trending/movie/day${KEY}`);
+    const movies = await response;
+    // console.log(movies.data);
     return movies.data;
 }
 
@@ -23,18 +24,18 @@ async function fetchTopMovies() {
 
 
 async function fetchMoviesByQuery(movie) {
-    const responce = await axios.get(`https://api.themoviedb.org/3/search/movie${KEY}${QUERY}&query=${movie}`);
-    const movies = await responce;
-    console.log(movies.data.results);
+    const response = await axios.get(`https://api.themoviedb.org/3/search/movie${KEY}${QUERY}&query=${movie}`);
+    const movies = await response;
+    // console.log(movies.data.results);
     return movies.data;
 }
 
 // fetchMoviesByQuery('pirates');
 
 async function fetchMovieById(movieId) {
-    const responce = await axios.get(`https://api.themoviedb.org/3/movie/${movieId}${KEY}&language=en-US`);
-    const movie = await responce;
-    console.log(movie.data);
+    const response = await axios.get(`https://api.themoviedb.org/3/movie/${movieId}${KEY}&language=en-US`);
+    const movie = await response;
+    // console.log(movie.data);
     return movie.data;
 }
 
