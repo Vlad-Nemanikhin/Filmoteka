b412134bceabcce8862cd8c897aa5317776603cf
Denis Zapara <dzapara24@gmail.com>
2021-08-12 23:38:05 +0300

    Переписал интерфейс localStorage на класс

--- src/js/localStorageAPI.js
+++ src/js/localStorageAPI.js
@@ -1,27 +1,26 @@
-const save = (key, value) => {
-  try {
-    const jsonForm = JSON.stringify(value);
-    localStorage.setItem(key, jsonForm);
-  } catch (error) {
-    console.error("Set state error: ", error.message);
-  }
-};
 
-const load = key => {
-  try {
-    const jsonForm = localStorage.getItem(key);
-    return jsonForm === null ? undefined : JSON.parse(jsonForm);
-  } catch (error) {
-    console.error("Get state error: ", error.message);
-  }
-};
+export default class {
 
-const remove = key => {
-  localStorage.removeItem(key)
+  save (key, value) {
+     try {
+      const jsonForm = JSON.stringify(value);
+      localStorage.setItem(key, jsonForm);
+    } catch (error) {
+      console.error("Set state error: ", error.message);
+    }
+  }
+
+  load (key) {
+    try {
+      const jsonForm = localStorage.getItem(key);
+      return jsonForm === null ? undefined : JSON.parse(jsonForm);
+    } catch (error) {
+      console.error("Get state error: ", error.message);
+    }
+  }
+
+  remove (key) {
+    localStorage.removeItem(key)
+  }
+
 }
-
-export default {
-  save,
-  load,
-  remove,
-};
