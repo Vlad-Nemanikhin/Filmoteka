55827dafa8f86af931400baf63b1fd8a031f716c
Anatolii-med <anatolii@github.com>
2021-08-13 17:48:11 +0300

    допилена модалка команды

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import './sass/main.scss';
 import ApiService from './js/movies-API-service';
 import article from './handlebars/article.hbs';
 import axios from 'axios';
-import Notiflix from "notiflix";
+import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
 import * as APIs from './js/movies-API-service';
-import modalHidden from './js/close-modal'
+import modalHidden from './js/close-modal';
+import './js/footer_modal';
 
 APIs.fetchGenres();
 modalHidden.closeModal();
-
-
