afb717f397d40d7e39babf7a6368ecc71779cb4a
Lera24 <bedulinavalera21@gmail.com>
2021-08-14 15:52:51 +0300

    решение конфликта подключения

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import './sass/main.scss';
-import ApiService from './js/movies-API-service';
 import article from './handlebars/article.hbs';
 import axios from 'axios';
 import Notiflix from "notiflix";
