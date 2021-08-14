036d608a51b2321c713aa3973ea1b693d9860627
Lera24 <bedulinavalera21@gmail.com>
2021-08-14 15:58:37 +0300

    333

--- src/index.js
+++ src/index.js
@@ -3,4 +3,3 @@ import article from './handlebars/article.hbs';
 import axios from 'axios';
 import Notiflix from "notiflix";
 import debounce from 'lodash.debounce';
-import * as APIs from './js/movies-API-service';
