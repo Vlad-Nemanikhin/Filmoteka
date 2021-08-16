9dc63d4526dd67e7b07d56b1522ec7bf1a367cb4
Vlad Nemanikhin <77212359+Vlad-Nemanikhin@users.noreply.github.com>
2021-08-16 17:55:39 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import './sass/main.scss';
-import article from './handlebars/article.hbs';
-import axios from 'axios';
-import Notiflix from 'notiflix';
-import debounce from 'lodash.debounce';
 import * as APIs from './js/movies-API-service';
 import modalHidden from './js/close-modal';
 import * as renderFilms from './js/renderFilmsFromSubmit';
-import { getYear } from 'date-fns';
 import changeHeader from './js/change-view-header';
 import './js/footer_modal';
 import switchTheme from './js/switch-theme';
 import { renderTopMovies, fetchTopMovies, getGenres, renderGenres } from './js/popular-movies.js';
+import * as topScrollBtn from './js/top-btn-scroll';
+import article from './handlebars/article.hbs';
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
+import { getYear } from 'date-fns';
 import Pagination from 'tui-pagination';
+import 'lazysizes';
 
 getGenres();
 APIs.fetchGenres();
