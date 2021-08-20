f8233bb54cebea19f5498a935e401bd7bb3d27a2
Vitalii Kovalenko <vitaliikovalenko23@gmail.com>
2021-08-20 15:24:56 +0300

    Update movies-API-service.js

--- src/js/common/movies-API-service.js
+++ src/js/common/movies-API-service.js
@@ -1,40 +1,33 @@
 import axios from 'axios';
 
+axios.defaults.baseURL = 'https://api.themoviedb.org';
 const KEY = '?api_key=130ddbced0917ef5d6e094c730cee47c';
 const QUERY = '&language=en&language=ru&include_adult=false';
 
 
 async function fetchGenres() {
-    const responce = await axios.get(`https://api.themoviedb.org/3/genre/movie/list${KEY}&language=en&language=ru`);
-    const genres = await responce;
-    return genres.data;
+    const responce = await axios.get(`/3/genre/movie/list${KEY}&language=en&language=ru`);;
+    return responce.data;
 }
 
-// fetchGenres();
 
 async function fetchTopMovies(page) {
-    const response = await axios.get(`https://api.themoviedb.org/3/trending/movie/day${KEY}&language=en&language=ru&page=${page}`);
-    const movies = await response;
-    return movies.data;
+    const response = await axios.get(`/3/trending/movie/day${KEY}&language=en&language=ru&page=${page}`);
+    return response.data;
 }
 
-// fetchTopMovies();
 
 
 async function fetchMoviesByQuery(movie, page) {
-    const responce = await axios.get(`https://api.themoviedb.org/3/search/movie${KEY}${QUERY}&query=${movie}&page=${page}`);
-    const movies = await responce;
-    return movies.data;
+    const responce = await axios.get(`/3/search/movie${KEY}${QUERY}&query=${movie}&page=${page}`);
+    return responce.data;
 }
 
-// fetchMoviesByQuery('pirates');
 
 async function fetchMovieById(movieId) {
-    const response = await axios.get(`https://api.themoviedb.org/3/movie/${movieId}${KEY}&language=en&language=ru`);
-    const movie = await response;
-    return movie.data;
+    const response = await axios.get(`/3/movie/${movieId}${KEY}&language=en&language=ru`);
+    return response.data;
 }
 
-// fetchMovieById();
 
 export {fetchGenres, fetchTopMovies, fetchMoviesByQuery, fetchMovieById}
